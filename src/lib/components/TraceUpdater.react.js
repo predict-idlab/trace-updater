541d0ac23b86f9a3b9e6653f8e6b99be087c8bd0
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-12-04 21:55:58 +0100

    :dizzy: new version

--- src/lib/components/TraceUpdater.react.js
+++ src/lib/components/TraceUpdater.react.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
-// used to store the previous layout obect
-var _prev_relayout = null;
-var _first_time = true;
+// function used to extract the '.'-string objects into expanded objects :)
+const transformObj = obj => {
+    return Object.keys(obj).reduce((acc, key) => {
+        if (key.indexOf('.') >= 0) {
+            const [parentKey, childKey] = key.split('.');
+            acc[parentKey] = acc[parentKey] || {};
+            acc[parentKey][childKey] = obj[key];
+        } else {
+            acc[key] = obj[key];
+        }
+        return acc;
+    }, {});
+}
+
+var _prev_layout = null;
 
 /**
  * TraceUpdater is a component which updates the trace-data of a plotly graph.
@@ -17,21 +30,17 @@ var _first_time = true;
  *       index under the `index` attribute
  */
 export default class TraceUpdater extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
     shouldComponentUpdate(nextProps) {
         var { updateData } = nextProps
         if (Array.isArray(updateData) && updateData.length > 1) {
-            if (_prev_relayout === null) {
-                _prev_relayout = updateData[0];
-                return true;
-            }
             // console.log("==============shouldComponentUpdate==============");
-            // console.log("prev layout: ", _prev_relayout);
-            // console.log("update layout: ", updateData[0]);
-            //  TODO -> use the set-state here :)
-            // do not update if the 
-            var rtrn = _prev_relayout != updateData[0];
+            var rtrn = _prev_layout != updateData[0];
             // console.log("rtrn: ", rtrn);
-            _prev_relayout = updateData[0];
             return rtrn;
         }
         return false;
@@ -43,10 +52,13 @@ export default class TraceUpdater extends Component {
         var graphDiv = document.getElementById(gdID)
 
         if (graphDiv && Array.isArray(updateData) && updateData.length > 1) {
+            _prev_layout = updateData[0]
             var trace, index, s_keys;
-
             graphDiv = graphDiv.getElementsByClassName('js-plotly-plot')[0];
             // console.log("graphDiv: ", graphDiv);
+
+            // appearantly I need to do this to copy this object properly
+            var prev_layout = JSON.parse(JSON.stringify(graphDiv.layout));
 
             if (sequentialUpdate) {
                 for (let i = 1; i < updateData.length; i++) {
@@ -70,20 +82,22 @@ export default class TraceUpdater extends Component {
                 };
             }
             else {
-                // update the trace in a betch
+                // Create a set-union of all the to-be-updated traces their 
+                // first-level keys
                 s_keys = new Set();
                 for (let i = 1; i < updateData.length; i++) {
-                    for (const key of Object.keys(updateData[i])) {
-                        s_keys.add(key);
-                    }
+                    Object.keys(updateData[i]).forEach(key => s_keys.add(key));
                 }
+                // Delete the visible key (as the back-end doesn't know what part of the
+                // front-end data actually is visible)
                 s_keys.delete("visible");
 
-                const singleUpdateData = {};
+                // new variable to store the updated layout in a compatible format to 
+                // call restyle only once
+                const singleUpdateData = { visible: [] };
                 for (const k of s_keys) {
                     singleUpdateData[k] = [];
                 }
-                singleUpdateData.visible = [];
                 const index_arr = [];
                 for (let i = 1; i < updateData.length; i++) {
                     trace = updateData[i];
@@ -94,13 +108,18 @@ export default class TraceUpdater extends Component {
                             singleUpdateData[k].push(trace[k]);
                         }
                     }
+                    // push the front-end data visible key
                     singleUpdateData.visible.push(graphDiv._fullData[trace.index].visible);
                     index_arr.push(trace.index);
                 };
                 Plotly.restyle(graphDiv, singleUpdateData, index_arr);
             }
-            // after the (either sequential or batch) data-updates, restyle the layout
-            Plotly.relayout(graphDiv, updateData[0]);
+
+            // TODO -> this relout re-calls this script - should be fixed
+            // update the layout - use a merge of the old-layout and a 
+            //      object "."extension of the new-layout e.g.
+            //      -> xaxis.autorange = true --> { xaxis : { autorange : true } }
+            Plotly.relayout(graphDiv, _.merge(prev_layout, transformObj(updateData[0])));
         };
         return <div id={id}></div>;
     }
