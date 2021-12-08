44429dc9bebf4cd3c917b0631a48c3313c992d9e
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-12-08 10:20:35 +0100

    :rocket: new version

--- src/TraceUpdater.jl
+++ src/TraceUpdater.jl
@@ -3,7 +3,7 @@ module TraceUpdater
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.3"
+const version = "0.0.4"
 
 include("jl/traceupdater.jl")
 
