508fcb146a9493d5c3c6048fca943704e1c8bb9f
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-03-10 10:25:27 +0100

    :fire: new version of traceupdater, w/ support for pattern matching callbacks

--- trace_updater/TraceUpdater.py
+++ trace_updater/TraceUpdater.py
@@ -6,28 +6,36 @@ from dash.development.base_component import Component, _explicitize_args
 class TraceUpdater(Component):
     """A TraceUpdater component.
     TraceUpdater is a component which updates the trace-data of a plotly graph.
-    It takes the properties
-    - gdID - which is the DCC.graph its id
-    - sequentialUpdate - if true, each trace is updated sequentially;
-        i.e. Plotly.restyle is called sequentially
-    - updatedData - A list whose:
-        * first object withholds the to-be applied layout
-        * second to ... object contain the updated trace data and its corresponding
-        index under the `index` attribute
+
     Parameters
     ----------
     id: string, optional
         The ID used to identify this component in Dash callbacks.
+
     gdID: string; required
-        The id of the graph-div whose traces should be.
-    sequentialUpdate: bool, default True
+        The id of the graph-div whose traces should be updated.
+
+        .. Note:
+
+            * if you use multiple graphs; each graph MUST have a unique id; otherwise we
+              cannot guarantee that resampling will work correctly.
+            * TraceUpdater will determine the html-graph-div by performing partial
+              matching on the \"id\" property (using `gdID`) of all divs with
+              classname=\"dash-graph\". It will select the first item of that match
+              list; so if multiple same graph-div IDs are used, or one graph-div-ID is a
+              subset of the other (partial matching) there is no guarantee that the
+              correct div will be selected.
+
+    sequentialUpdate: boolean, default False
         Bool indicating whether the figure should be redrawn sequentially
         (i.e.) calling the restyle multiple times or at once. (still needs
         to be determined which is faster has the lowest memory peak).
-    updateData (list; optional):
+
+    updateData: list, optional
         The data to update the graph with, must contain the `index`
         property for each trace; either a list of dict-traces or a single
         trace.
+
     """
 
     @_explicitize_args
