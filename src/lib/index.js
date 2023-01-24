4ced5b17019874ca920cfed29c55b74edda1f0fe
jonasvdd <jonvdrdo.vanderdonckt@ugent.be>
2023-01-24 21:14:24 +0100

    :sparkles: first version of layoutUpdater

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import TraceUpdater from './components/TraceUpdater.react';
+import LayoutUpdater from './components/LayoutUpdater.react';
 
 export {
-    TraceUpdater
+    TraceUpdater,
+    LayoutUpdater
 };
